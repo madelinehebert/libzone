b6108d49c7e98e6af486f585fa9253ec74a80b98
madelinehebert <madelinehebert@proton.me>
2024-09-08 11:46:34 +0000

    Began work on zone verification functions.

--- zone.go
+++ zone.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -20,11 +21,11 @@ type Zone struct {
 	ZonePath string
 	AutoBoot bool
 	IpType   IpType
-	AttrList map[byte]any
-	Dataset  map[byte]any
-	Device   map[byte]any
-	Fs       map[byte]any
-	Net      map[byte]any
+	AttrList map[int]any
+	Dataset  map[int]any
+	Device   map[int]any
+	Fs       map[int]any
+	Net      map[int]any
 }
 
 // Function to create a new zone struct
@@ -34,12 +35,51 @@ func (z *Zone) Init(name string) *Zone {
 	z.State = State.Incomplete()
 	z.ZonePath = ""
 	z.AutoBoot = false
-	z.AttrList = make(map[byte]any)
-	z.Dataset = make(map[byte]any)
-	z.Device = make(map[byte]any)
-	z.Fs = make(map[byte]any)
-	z.Net = make(map[byte]any)
+	z.AttrList = make(map[int]any)
+	z.Dataset = make(map[int]any)
+	z.Device = make(map[int]any)
+	z.Fs = make(map[int]any)
+	z.Net = make(map[int]any)
 	return z
+}
+
+// Function to verify a zone's creation state - returns an error if any condition is violated.
+// This fucntion starts by confirming the most bare bones configuration for a zone has been provided.
+// Then, it moves on to confirm each map of properties.
+func (z *Zone) Verify() error {
+	//Start checking the individual fields of the struct
+
+	//Zone name checks
+	if len(z.Name) < 1 { //If zone name is too short
+		return errors.New("err: zone name must be at least one character long.")
+	} else if len(z.Name) > 63 { //If zone name is too long
+		return errors.New("err: zone name must be less than 64 characters.")
+	} else if ok := regexp.MustCompile(`^[a-zA-Z0-9_.-]*$`).MatchString(z.Name); !ok { //If zone name does not contain legal characters
+		return errors.New("err: zone contains an illegal character. Please only use the following selection of characters for a zone name: 'A-Za-z0-9_.-'")
+	} else if z.Name[0:4] == "SUNW" { //If zone name begins with SUNW
+		return errors.New("err: zone name must not begin with the letter sequence: 'SUNW'.")
+	} else if z.Name == "global" { //If zone name is 'global'
+		return errors.New("err: zone name must not contain: 'global'.")
+	} else {
+		//Ensure zone name is not in use
+		if err := IsZoneNameInUse(z.Name); err != nil {
+			return err
+		}
+	}
+
+	//Zone brand check
+	if strings.Contains(z.Brand.String(), "Unknown") {
+		return errors.New("err: invalid brand with integer id: " + z.Brand.String())
+	}
+
+	//Zone state check
+	if z.State != State.Incomplete() && z.State != State.Configured() {
+		return errors.New("err: zone is not in valid state to require verification: " + z.State.String())
+	}
+
+	//Set state to configured and return nil
+	z.State = State.Configured()
+	return nil
 }
 
 // Function to print out the zone's information
@@ -70,19 +110,19 @@ func (z *Zone) Info() {
 		case reflect.TypeOf(true):
 			fmt.Printf("%20s: %-20v\n", zStructType.Field(i).Name, zStructValue.Field(i).Interface().(bool))
 
-		//If we have a map[byte]any
-		case reflect.TypeOf(make(map[byte]any)):
+		//If we have a map[int]any
+		case reflect.TypeOf(make(map[int]any)):
 
 			//Ensure we aren't trying to print an empty value
-			if len(zStructValue.Field(i).Interface().(map[byte]any)) != 0 {
+			if len(zStructValue.Field(i).Interface().(map[int]any)) != 0 {
 				//Loop over values
-				for key, _ := range zStructValue.Field(i).Interface().(map[byte]any) {
+				for key, _ := range zStructValue.Field(i).Interface().(map[int]any) {
 
 					//Print item header with number
 					fmt.Printf("%20s: %-20s\n", zStructType.Field(i).Name+" #"+strconv.Itoa(int(key)+1), "")
 
 					//Loop over sub values
-					for subKey, subVal := range zStructValue.Field(i).Interface().(map[byte]any)[key].(*Property).Value.(map[string]any) {
+					for subKey, subVal := range zStructValue.Field(i).Interface().(map[int]any)[key].(*Property).Value.(map[string]any) {
 						//Check type
 						switch reflect.TypeOf(subVal) {
 						//string
@@ -120,111 +160,155 @@ func (z *Zone) Info() {
 	}
 }
 
-// Function to add a property type to a zone
-func (z *Zone) Add(p string) error {
+// Function to add a property type to a zone - returns an int of the map key it sits at, and error if an invalid property is entered
+// Errors are also returned if 256 places in the map are occupied
+func (z *Zone) Add(p string) (int, error) {
 	p = strings.ToLower(p)
 	switch p {
 	case "attrlist":
-		z.AttrList[byte(len(z.AttrList))] = (&Property{}).AttrList()
+		index := len(z.AttrList)
+		//Cap number of items to 256
+		if index+1 > 255 {
+			return -2, errors.New("attrlist limit of 256 has been reached")
+		} else {
+			z.AttrList[index] = (&Property{}).AttrList()
+			return index, nil
+		}
+
 	case "dataset":
-		z.Dataset[byte(len(z.Dataset))] = (&Property{}).Dataset()
+		index := len(z.Dataset)
+		//Cap number of items to 256
+		if index+1 > 255 {
+			return -2, errors.New("dataset limit of 256 has been reached")
+		} else {
+			z.Dataset[index] = (&Property{}).Dataset()
+			return index, nil
+		}
 	case "device":
-		z.Device[byte(len(z.Device))] = (&Property{}).Device()
+		index := len(z.Device)
+		//Cap number of items to 256
+		if index+1 > 255 {
+			return -2, errors.New("device limit of 256 has been reached")
+		} else {
+			z.Device[index] = (&Property{}).Device()
+			return index, nil
+		}
 	case "fs":
-		z.Fs[byte(len(z.Fs))] = (&Property{}).Fs()
+		index := len(z.Fs)
+		//Cap number of items to 256
+		if index+1 > 255 {
+			return -2, errors.New("fs limit of 256 has been reached")
+		} else {
+			z.Fs[index] = (&Property{}).Fs()
+			return index, nil
+		}
 	case "net":
-		z.Net[byte(len(z.Net))] = (&Property{}).Net()
+		index := len(z.Net)
+		//Cap number of items to 256
+		if index+1 > 255 {
+			return -2, errors.New("net limit of 256 has been reached")
+		} else {
+			z.Net[index] = (&Property{}).Net()
+			return index, nil
+		}
 	default:
-		return errors.New("Unknown property: " + p)
+		return -1, errors.New("Unknown property: " + p)
 	}
 
-	return nil
 }
 
 // Function to configure a device - The 'item' is the property name, the 'id' is the index of that property type,
 // the 'key' is the field, and the 'val' is the value of the field. Invalid indices return errors.
-func (z *Zone) Configure(item string, id byte, key string, val string) error {
-	item = strings.ToLower(item)
-	switch item {
-	//Configure Attr List
-	case "attrlist":
-		//Check length of map
-		if len(z.AttrList) >= int(id)+1 {
-			if err := z.AttrList[id].(*Property).Set(key, val); err != nil {
-				return err
+func (z *Zone) Configure(item string, id int, key string, val string) error {
+	//Limit number of items to 256 - then fork to configuring if index is within range
+	if id < 0 || id > 255 {
+		return errors.New("Invalid index: " + strconv.Itoa(id))
+	} else {
+		item = strings.ToLower(item)
+		switch item {
+		//Configure Attr List
+		case "attrlist":
+			//Check length of map
+			if len(z.AttrList) >= int(id)+1 {
+				if err := z.AttrList[id].(*Property).Set(key, val); err != nil {
+					return err
+				} else {
+					return nil
+				}
 			} else {
-				return nil
+				return errors.New("invalid index")
 			}
-		} else {
-			return errors.New("invalid index")
-		}
 
-	//Configure Dataset
-	case "dataset":
-		//Check length of map
-		if len(z.Dataset) >= int(id)+1 {
-			if err := z.Dataset[id].(*Property).Set(key, val); err != nil {
-				return err
+		//Configure Dataset
+		case "dataset":
+			//Check length of map
+			if len(z.Dataset) >= int(id)+1 {
+				if err := z.Dataset[id].(*Property).Set(key, val); err != nil {
+					return err
+				} else {
+					return nil
+				}
 			} else {
-				return nil
+				return errors.New("invalid index")
 			}
-		} else {
-			return errors.New("invalid index")
-		}
-	//Configure Device
-	case "device":
-		//Check length of map
-		if len(z.Device) >= int(id)+1 {
-			if err := z.Device[id].(*Property).Set(key, val); err != nil {
-				return err
+		//Configure Device
+		case "device":
+			//Check length of map
+			if len(z.Device) >= int(id)+1 {
+				if err := z.Device[id].(*Property).Set(key, val); err != nil {
+					return err
+				} else {
+					return nil
+				}
 			} else {
-				return nil
+				return errors.New("invalid index")
 			}
-		} else {
-			return errors.New("invalid index")
-		}
-	//Configure Fs
-	case "fs":
-		//Check length of map
-		if len(z.Fs) >= int(id)+1 {
-			if err := z.Fs[id].(*Property).Set(key, val); err != nil {
-				return err
+		//Configure Fs
+		case "fs":
+			//Check length of map
+			if len(z.Fs) >= int(id)+1 {
+				if err := z.Fs[id].(*Property).Set(key, val); err != nil {
+					return err
+				} else {
+					return nil
+				}
 			} else {
-				return nil
+				return errors.New("invalid index")
 			}
-		} else {
-			return errors.New("invalid index")
-		}
-	//Configure Net
-	case "net":
-		//Check length of map
-		if len(z.Net) >= int(id)+1 {
-			if err := z.Net[id].(*Property).Set(key, val); err != nil {
-				return err
+		//Configure Net
+		case "net":
+			//Check length of map
+			if len(z.Net) >= int(id)+1 {
+				if err := z.Net[id].(*Property).Set(key, val); err != nil {
+					return err
+				} else {
+					return nil
+				}
 			} else {
-				return nil
+				return errors.New("invalid index")
 			}
-		} else {
-			return errors.New("invalid index")
+		//Handle unknown property
+		default:
+			return errors.New("unknown property: " + item)
 		}
-	//Handle unknown property
-	default:
-		return errors.New("unknown property: " + item)
 	}
 
 }
 
 // Function to configure a device
-func (z *Zone) ConfigureDevice(id byte, key string, val string) error {
+
+/*
+func (z *Zone) ConfigureDevice(id int, key string, val string) error {
 	if err := z.Device[id].(*Property).Set(key, val); err != nil {
 		return err
 	} else {
 		return nil
 	}
 }
+*/
 
 // Function to return the value of a specified net property
-func (z *Zone) ReturnAttrList(propertyIndex byte, field string) any {
+func (z *Zone) ReturnAttrList(propertyIndex int, field string) any {
 	if z.AttrList != nil {
 		return z.AttrList[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
@@ -233,7 +317,7 @@ func (z *Zone) ReturnAttrList(propertyIndex byte, field string) any {
 }
 
 // Function to return the value of a specified net property
-func (z *Zone) ReturnDataset(propertyIndex byte, field string) any {
+func (z *Zone) ReturnDataset(propertyIndex int, field string) any {
 	if z.Dataset != nil {
 		return z.Dataset[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
@@ -242,7 +326,7 @@ func (z *Zone) ReturnDataset(propertyIndex byte, field string) any {
 }
 
 // Function to return the value of a specified net property
-func (z *Zone) ReturnDevice(propertyIndex byte, field string) any {
+func (z *Zone) ReturnDevice(propertyIndex int, field string) any {
 	if z.Device != nil {
 		return z.Device[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
@@ -251,7 +335,7 @@ func (z *Zone) ReturnDevice(propertyIndex byte, field string) any {
 }
 
 // Function to return the value of a specified net property
-func (z *Zone) ReturnFs(propertyIndex byte, field string) any {
+func (z *Zone) ReturnFs(propertyIndex int, field string) any {
 	if z.Net != nil {
 		return z.Fs[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
@@ -260,7 +344,7 @@ func (z *Zone) ReturnFs(propertyIndex byte, field string) any {
 }
 
 // Function to return the value of a specified net property
-func (z *Zone) ReturnNet(propertyIndex byte, field string) any {
+func (z *Zone) ReturnNet(propertyIndex int, field string) any {
 	if z.Net != nil {
 		return z.Net[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
@@ -287,9 +371,6 @@ func (z *Zone) Shutdown() error { return nil }
 // Function to reboot a zone
 func (z *Zone) Reboot() error { return nil }
 
-// Function to verify a zone's configuration
-func (z *Zone) Verify() error { return nil }
-
 // Function to install a zone
 func (z *Zone) Install() error { return nil }
 
@@ -304,5 +385,15 @@ func (z *Zone) Attach() error { return nil }
 
 // Function to detach a zone
 func (z *Zone) Detach() error { return nil }
+
+// Check if a zone name is in use
+func IsZoneNameInUse(name string) error {
+	//Check the zone name
+	if false {
+		return errors.New("zone name in use")
+	} else {
+		return nil
+	}
+}
 
 //func (z *Zone) x() error    {return nil}
--- zone_return.go
+++ zone_return.go
@@ -1,14 +1,14 @@
 package libzone
 
 // Function to return the value of a specified zone's property
-func Return(i any, propertyIndex byte, field string) any {
+func Return(i any, propertyIndex int, field string) any {
 	// Defer panic
 	defer func(any) {
 		recover()
 	}(nil)
 
-	if i.(map[byte]any) != nil {
-		return i.(map[byte]any)[propertyIndex].(*Property).Value.(map[string]any)[field]
+	if i.(map[int]any) != nil {
+		return i.(map[int]any)[propertyIndex].(*Property).Value.(map[string]any)[field]
 	} else {
 		return nil
 	}
